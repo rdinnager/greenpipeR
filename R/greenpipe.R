275c83ef5d91b8c782d638f749e036fa986fefec
rdinnager <r.dinnage@gmail.com>
2014-06-28 15:47:53 +0800

    this doesn't even come close to working yet

--- R/greenpipe.R
+++ R/greenpipe.R
@@ -1,10 +1,46 @@
+#' Arm a value with a function which fires when evaluated.
+#'
+#' @param expr an expression
+#' @param fun a function of one argument.
+#'
+#' @return an expression which when evaluated returns the result of expr,
+#' after evaluating fun(expr).
+#' @export
+#' @details Thanks very much to Stefan Milton Bache for providing the code to replace the 
+#' now defunct \code{pipe_with} function which uses \code{arm}, which is necessary for \code{\%W>\%} 
+#' to function. Retrieved from this gist: \url{https://gist.github.com/smbache/86fe703fa46e39df33ea}
+arm <- function(expr, fun, count)
+{
+  substitute({
+    expr %T>% fun
+  },
+  list(
+    fun = substitute(call("fun", count)),
+    expr = substitute(expr)
+  )
+  )
+}
+
+pipe_with <- function(expr) function(lhs, rhs)
+{
+  parent <- parent.frame()
+  print(substitute(lhs))
+  counter <- gregexpr("%W>%", substitute(lhs), fixed = TRUE)[[1]]
+  if (counter[1] == -1) count <- 1 else count <- length(counter) + 1
+  print(count)
+  lhs <- eval(call("arm", substitute(lhs), expr, count), parent, parent)
+  eval(call("%>%", lhs, rhs), parent, parent)
+}
+
 warp <- local({
   timing <- proc.time()[3]
-  function(x) {
+  function(x, count) {
     soundtime <- proc.time()[3] - timing
     cl <- match.call()
-    counter <- gregexpr("%W>%", cl[[2]], fixed = TRUE)[[1]]
-    if (counter[1] == -1) count <- 1 else count <- length(counter) + 1
+    #counter <- gregexpr("%W>%", cl[[2]], fixed = TRUE)[[1]]
+    #if (counter[1] == -1) count <- 1 else count <- length(counter) + 1
+    print(cl)
+    print(soundtime)
     if (count == 1 || soundtime > 0.6) {
       beep(system.file("sound", "smb_pipe.wav", package="greenpipeR"))
       timing <<- proc.time()[3]
